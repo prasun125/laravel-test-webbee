5b121bce5566ef2b1663ec25a9f9b8ed13d333f0
Prasun Paul Chowdhury <prasun@novatree.com>
2021-05-02 21:32:35 +0530

    getEventsWithWorkshops function that returns all events with workshops

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -97,7 +97,8 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        //throw new \Exception('implement in coding task 1');
+        return Event::with('workshops')->get();
     }
 
 
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Workshop;
 
 class Event extends Model
 {
+	public function workshops(){
+	   return $this->hasMany(Workshop::class);
+	}
 }
